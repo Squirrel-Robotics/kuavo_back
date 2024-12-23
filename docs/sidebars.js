ff4a7623bf068a27b8668587edadd7c31182ec49
kuavo_CI <kuavo_CI@50_eb_f6_bd_54_c1>
2024-12-23 10:32:06 +0800

    sync https://www.lejuhub.com/highlydynamic/kuavo-ros-control/commit/2faa308f710cc16758831354e7364ca127d64c1d

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -35,7 +35,7 @@ const sidebars = {
       items: [
         'basic_usage/kuavo-ros-control/docs/3开发接口/仿真环境使用',
         'basic_usage/kuavo-ros-control/docs/3开发接口/SDK介绍',
-        'basic_usage/kuavo-ros-control/docs/3开发接口/sdk_use',
+        'basic_usage/kuavo-ros-control/docs/3开发接口/接口使用文档',
       ],
     },
     {
@@ -49,12 +49,13 @@ const sidebars = {
             'basic_usage/kuavo-ros-control/docs/4功能案例/通用案例/自定义启动案例',
             'basic_usage/kuavo-ros-control/docs/4功能案例/通用案例/H12遥控器使用开发案例',
             'basic_usage/kuavo-ros-control/docs/4功能案例/通用案例/VR使用开发案例',
-            'basic_usage/kuavo-ros-control/docs/4功能案例/通用案例/单步控制案例(落足点规划)',
+            'basic_usage/kuavo-ros-control/docs/4功能案例/通用案例/落足点规划案例',
             'basic_usage/kuavo-ros-control/docs/4功能案例/通用案例/二维码检测使用案例',
             'basic_usage/kuavo-ros-control/docs/4功能案例/通用案例/灵巧手手势使用案例',
             'basic_usage/kuavo-ros-control/docs/4功能案例/通用案例/路径轨迹规划案例',
             'basic_usage/kuavo-ros-control/docs/4功能案例/通用案例/数据采集案例',
             'basic_usage/kuavo-ros-control/docs/4功能案例/通用案例/yolov8目标检测案例',
+            'basic_usage/kuavo-ros-control/docs/4功能案例/通用案例/手臂逆运动学案例',
           ],
         },
         {
@@ -62,7 +63,7 @@ const sidebars = {
           label: '扩展案例',
           items: [
             'basic_usage/kuavo-ros-control/docs/4功能案例/拓展案例/大模型使用案例',
-            'basic_usage/kuavo-ros-control/docs/4功能案例/拓展案例/机器人导航',
+            'basic_usage/kuavo-ros-control/docs/4功能案例/拓展案例/机器人导航案例',
           ],
         },
         'basic_usage/kuavo-ros-control/docs/4功能案例/案例目录',
@@ -72,7 +73,7 @@ const sidebars = {
       type: 'category',
       label: 'Changelog',
       items: [
-        'basic_usage/kuavo_ros1_workspace/CHANGELOG'
+        'basic_usage/kuavo-ros-control/CHANGELOG'
       ],
     },
   ],
