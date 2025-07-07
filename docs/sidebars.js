9b6bfe2f2ead228301269c5d01479a40a6aa84c4
kuavo_CI <kuavo_CI@50_eb_f6_bd_54_c1>
2025-07-07 12:05:28 +0800

    sync https://www.lejuhub.com/highlydynamic/kuavo-ros-control/commit/5e992c976ee4fb0797623be2b10322d2b6cdfe80

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -107,6 +107,7 @@ const sidebars = {
             'basic_usage/kuavo-ros-control/docs/5功能案例/通用案例/二维码抓取水瓶案例',
             'basic_usage/kuavo-ros-control/docs/5功能案例/通用案例/基于人脸识别的简易语音交互案例',
             'basic_usage/kuavo-ros-control/docs/5功能案例/通用案例/全身打太极动作案例',
+            'basic_usage/kuavo-ros-control/docs/5功能案例/通用案例/策略模块搬箱子案例',
           ],
         },
         {
