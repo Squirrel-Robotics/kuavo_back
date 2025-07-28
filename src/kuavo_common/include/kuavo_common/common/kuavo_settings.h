5c9f54dc0af9454bb119c7ebd60a26aac565ea9f
kuavo_CI <kuavo_CI@50_eb_f6_bd_54_c1>
2025-07-28 20:12:23 +0800

    sync https://www.lejuhub.com/highlydynamic/kuavo-ros-control/commit/5a2776da404cfb17e1eb34895115a837823bef88

--- src/kuavo_common/include/kuavo_common/common/kuavo_settings.h
+++ src/kuavo_common/include/kuavo_common/common/kuavo_settings.h
@@ -6,7 +6,7 @@
 #include <Eigen/Core>
 #include "kuavo_common/common/robot_state.h"
 #include "kuavo_common/common/json_config_reader.hpp"
-
+#include "kuavo_common/common/common.h"
 namespace HighlyDynamic
 {
 
@@ -153,8 +153,8 @@ namespace HighlyDynamic
             motors_exist.resize(num_joints);
             motors_disable.resize(num_joints);
         }
-        std::string getEcmasterType(int robot_version_int=40);
-        std::string getIMUType(int robot_version_int = 40);
+        std::string getEcmasterType(RobotVersion rb_version = RobotVersion(4, 0));
+        std::string getIMUType(RobotVersion rb_version = RobotVersion(4, 0));
 
     };
 
