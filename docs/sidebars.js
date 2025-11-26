5b06233d2d4974a2315a1a1ac55751b938e20310
kuavo_CI <kuavo_CI@50_eb_f6_bd_54_c1>
2025-11-27 00:42:38 +0800

    sync https://www.lejuhub.com/highlydynamic/kuavo-ros-control/commit/e582172c1d8d3757b59bfdb92a1af173eade6ff2

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -122,9 +122,8 @@ const sidebars = {
               items: [
                 'basic_usage/kuavo-ros-control/docs/5功能案例/拓展案例/模仿学习使用案例/案例概述',
                 'basic_usage/kuavo-ros-control/docs/5功能案例/拓展案例/模仿学习使用案例/数据采集',
-                'basic_usage/kuavo-ros-control/docs/5功能案例/拓展案例/模仿学习使用案例/数据转换',
-                'basic_usage/kuavo-ros-control/docs/5功能案例/拓展案例/模仿学习使用案例/模型训练',
-                'basic_usage/kuavo-ros-control/docs/5功能案例/拓展案例/模仿学习使用案例/模型部署',
+                'basic_usage/kuavo-ros-control/docs/5功能案例/拓展案例/模仿学习使用案例/环境部署',
+                'basic_usage/kuavo-ros-control/docs/5功能案例/拓展案例/模仿学习使用案例/使用方法',
               ],
             },
             'basic_usage/kuavo-ros-control/docs/5功能案例/拓展案例/手腕相机抓取放置案例',
